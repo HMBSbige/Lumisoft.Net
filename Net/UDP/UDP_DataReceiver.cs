af130b3b023fd45fc44eb1378b4446573581a035
ivar <ivar@c322367f-d4df-4f43-9d27-36dfb190b512>
2017-06-24 16:43:37 +0000

    Increase receive buffer to 8000kb.
    git-svn-id: https://svn.lumisoft.ee:8443/svn/LumiSoft_Net/trunk@690 c322367f-d4df-4f43-9d27-36dfb190b512

--- Net/UDP/UDP_DataReceiver.cs
+++ Net/UDP/UDP_DataReceiver.cs
@@ -19,7 +19,7 @@ namespace LumiSoft.Net.UDP
         private bool                 m_IsRunning   = false;
         private Socket               m_pSocket     = null;
         private byte[]               m_pBuffer     = null;
-        private int                  m_BufferSize  = 1400;
+        private int                  m_BufferSize  = 8000;
         private SocketAsyncEventArgs m_pSocketArgs = null;
         private UDP_e_PacketReceived m_pEventArgs  = null;
 
